25a64745bd9022a820672ce8c4103a711cc83c37
Carl Johnson <me@carlmjohnson.net>
2021-09-05 18:15:54 -0400

    Add Trace (#3)

--- prefix.go
+++ prefix.go
@@ -1,11 +1,30 @@
 package errutil
 
-import "fmt"
+import (
+	"fmt"
+	"path/filepath"
+	"runtime"
+)
 
 // Prefix will prefix an error with a fixed string if it is non-nil.
 func Prefix(errp *error, prefixformat string, a ...interface{}) {
 	if err := *errp; err != nil {
 		prefix := fmt.Sprintf(prefixformat, a...)
+		*errp = fmt.Errorf("%s: %w", prefix, err)
+	}
+}
+
+// Trace prefixes an error with caller information if the error is not nil.
+func Trace(errp *error) {
+	if err := *errp; err != nil {
+		pc, file, line, ok := runtime.Caller(1)
+		prefix := "debug information not available"
+		if ok {
+			f := runtime.FuncForPC(pc)
+			file = filepath.Base(file)
+			prefix = fmt.Sprintf("problem in %s (%s:%d)",
+				f.Name(), file, line)
+		}
 		*errp = fmt.Errorf("%s: %w", prefix, err)
 	}
 }
--- prefix_example_test.go
+++ prefix_example_test.go
@@ -7,7 +7,50 @@ import (
 	"github.com/carlmjohnson/errutil"
 )
 
+func traceErr1(ok bool) (err error) {
+	defer errutil.Trace(&err)
+	if !ok {
+		return errors.New("oh no!")
+	}
+	return nil
+}
+
+func traceErr2(x, y int) (err error) {
+	defer errutil.Trace(&err)
+	if x+y > 1 {
+		return errors.New("uh oh!")
+	}
+	return nil
+}
+
+func ExampleTrace() {
+	fmt.Println(traceErr1(true))
+	fmt.Println(traceErr1(false))
+	fmt.Println(traceErr2(1, -1))
+	fmt.Println(traceErr2(1, 1))
+	// Output:
+	// <nil>
+	// problem in github.com/carlmjohnson/errutil_test.traceErr1 (prefix_example_test.go:13): oh no!
+	// <nil>
+	// problem in github.com/carlmjohnson/errutil_test.traceErr2 (prefix_example_test.go:21): uh oh!
+}
+
 func ExamplePrefix() {
+	maybeErr1 := func(ok bool) (err error) {
+		defer errutil.Prefix(&err, "maybeErr1")
+		if !ok {
+			return errors.New("oh no!")
+		}
+		return nil
+	}
+
+	maybeErr2 := func(x, y int) (err error) {
+		defer errutil.Prefix(&err, "maybeErr2(%d, %d)", x, y)
+		if x+y > 1 {
+			return errors.New("uh oh!")
+		}
+		return nil
+	}
 	fmt.Println(maybeErr1(true))
 	fmt.Println(maybeErr1(false))
 	fmt.Println(maybeErr2(1, -1))
@@ -17,20 +60,4 @@ func ExamplePrefix() {
 	// maybeErr1: oh no!
 	// <nil>
 	// maybeErr2(1, 1): uh oh!
-}
-
-func maybeErr1(ok bool) (err error) {
-	defer errutil.Prefix(&err, "maybeErr1")
-	if !ok {
-		return errors.New("oh no!")
-	}
-	return nil
-}
-
-func maybeErr2(x, y int) (err error) {
-	defer errutil.Prefix(&err, "maybeErr2(%d, %d)", x, y)
-	if x+y > 1 {
-		return errors.New("uh oh!")
-	}
-	return nil
 }
