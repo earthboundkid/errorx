bb72d723936c843b88f69f259293bb4eaf86f7af
Carl Johnson <me@carlmjohnson.net>
2022-04-15 14:34:11 -0400

    gofmt -w -r 'interface{} -> any' .

--- prefix.go
+++ prefix.go
@@ -7,7 +7,7 @@ import (
 )
 
 // Prefix will prefix an error with a fixed string if it is non-nil.
-func Prefix(errp *error, prefixformat string, a ...interface{}) {
+func Prefix(errp *error, prefixformat string, a ...any) {
 	if err := *errp; err != nil {
 		prefix := fmt.Sprintf(prefixformat, a...)
 		*errp = fmt.Errorf("%s: %w", prefix, err)
